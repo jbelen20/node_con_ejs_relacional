68b4b6b9ba4d0d5227f404a6a6830c8e92f7f83b
jbelen20 <judithcarrenovega@gmail.com>
2024-08-04 20:08:55 -0400

    first commit

---
+++ src/controllers/customerController.js
@@ -0,0 +1,29 @@
+const controller = {};
+
+controller.list = (req, res) => {
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM customer', (err, customers) => {
+            if(err){
+                res.json(err)
+                //ver manejo de errores con next
+            }
+            res.render('customers', {
+                data: customers
+            })
+        })
+    });
+};
+
+
+controller.save = (req, res) => {
+    const data = req.body;
+    req.getConnection((err,conn)=>{
+        conn.query('INSERT INTO customer set ? ',[data], (err, customer) => {
+            console.log(customer)
+            res.send('works')
+        })
+    })
+};
+
+
+module.exports = controller;

2de0c7444368786d7f2ca7f2a007f8ac039952af
jbelen20 <judithcarrenovega@gmail.com>
2024-09-08 12:54:08 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(morgan('dev'))
 app.use(myConnection(mysql,{
     host:'localhost',
     user: 'root',
-    password:'cafeina25',
+    password:'',
     port:'3306',
     database: 'crudnodejsmysql'
 },'single'));
